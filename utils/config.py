d6179b8fb1ff63a9397493304fc0119618207948
RuslanGit753 <ruslan999808999@yandex.ru>
2025-05-21 16:58:29 +0300

    Stop tracking utils folder and config.py, add to .gitignore

--- utils/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    BASE_URL = 'https://job-frontend-psi.vercel.app/vacancies'
-
-    my_appl_name_1 = 'lol126lols@gmail.com'
-    my_appl_pas_1 = 'Enter1443'
-
-    my_appl_name_2 = 'rus.serebryakov00@mail.ru'
-    my_appl_pas_2 = 'Enter15676'
